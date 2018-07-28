76724ecae8b73f19f42d3b3aed3e2325116546a2
Jason (koad) Zvaniga <jason@zvaniga.com>
2018-07-28 18:18:30 -0400

    added sample config file

---
+++ sample-config.js
@@ -0,0 +1,31 @@
+var config = {};
+
+config.prefix = "";
+config.homeId = '470727642170261504';
+config.copyright = "© kingofalldata.com";
+
+// config.owner = '218373350643335168';
+config.owner = ['218373350643335168', '470378053198020619', '142733254154387456']; // Multiple Owners
+
+config.DEBUG = false;
+config.clearConsole = false;
+
+config.unknownCommandResponse = false;
+config.disableEveryone = true;
+
+config.color = {};
+config.color.brand = 2053826;
+config.color.alert = 11996943;
+
+config.discord = {};
+config.discord.token = process.env.TOKEN || 'PUT YOUR TOKEN IN HERE AFTER MOVING THIS FILE SOMEWHERE SAFE';  // testy
+
+config.mongo = {};
+config.mongo.host = 'localhost';
+config.mongo.port = 27017;
+config.mongo.db = 'testy-discord-bot';
+
+config.web = {};
+config.web.port = process.env.WEBPORT || 8080;
+
+module.exports = config;
